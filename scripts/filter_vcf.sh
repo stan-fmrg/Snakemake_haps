e5a6c1c042e964fe30c400a2d55f980c4498575f
stan-fmrg <43649544+stan-fmrg@users.noreply.github.com>
2018-09-28 17:37:02 +0100

    Delete filter_vcf.sh

--- scripts/filter_vcf.sh
+++
@@ -1,3 +0,0 @@
-vcftools --vcf $1 --out out.1 --minDP 10 --minGQ 30 --recode --recode-INFO-all
-vcftools --vcf out.1.recode.vcf --out out.2 --max-missing 0.9 --recode --recode-INFO-all
-vcfallelicprimitives 
